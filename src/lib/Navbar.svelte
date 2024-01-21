e37057e25382fe7edbbc1dcb4d73cc45e226235e
toftpokk <pakkapol.20100@gmail.com>
2024-01-21 16:46:01 +0700

    Referral table

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
+    import UserMenu from "./UserMenu.svelte";
+    import usericon from '$lib/user.svg'
+    import chevronicon from '$lib/chevron-down.svg'
 
     export let title : string;
     export let username : string = "";
@@ -7,16 +10,19 @@
         goto("/logout")
     }
 </script>
-<nav class="flex justify-between px-8 py-4">
+<nav class="flex justify-between px-8 py-4 border-b mb-4">
     <div class="flex gap-2">
         <a class="mt-1" href="/">
             <img src="/icons/home.svg" alt="home" class="w-8"/>
         </a>
         
-        <h2 class="text-3xl font-bold pt-1">{title}</h2>
+        <h2 class="text-2xl font-bold pt-1">{title}</h2>
     </div>
     <div class="my-auto">
-        <span class="text-lg">User: {username}</span>
-        <button class="btn" on:click={handleLogout}>Log Out</button>
+        <UserMenu class="">
+            <img src={usericon} alt="user" class="mr-1"/>
+            {username}
+            <img src={chevronicon} alt="chevron" class="ml-1 w-4"/>
+        </UserMenu>
     </div>
 </nav>
---
+++ src/lib/UserMenu.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+	import { cn } from "$lib/utils";
+	import type { HTMLTableAttributes } from "svelte/elements";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+
+	// type $$Props = HTMLTableAttributes;
+	let className : string = "";
+    let url = "/";
+	export { className as class, url };
+  </script>
+  
+  <DropdownMenu.Root>
+    <!-- <DropdownMenu.Trigger>Open</DropdownMenu.Trigger> -->
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button builders={[builder]} variant="outline" class={cn("w-full caption-bottom text-sm", className)}>
+        <slot/>
+    </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content class="w-56">
+      <DropdownMenu.Group>
+        <DropdownMenu.Item href={url}>
+            Logout
+          </DropdownMenu.Item>
+      </DropdownMenu.Group>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
