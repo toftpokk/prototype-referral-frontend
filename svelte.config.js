ce62ecf46722fbda749d0ce407764dfb2adc557d
toftpokk <pakkapol.20100@gmail.com>
2024-01-26 23:21:41 +0700

    Node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
