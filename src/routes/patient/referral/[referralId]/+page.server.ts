06235650d2c9c200f06c827c0222bc1aa9ae971d
toftpokk <pakkapol.20100@gmail.com>
2024-01-26 20:12:48 +0700

    Consent submission

--- src/routes/patient/referral/[referralId]/+page.server.ts
+++ src/routes/patient/referral/[referralId]/+page.server.ts
@@ -1,5 +1,6 @@
 import { PUBLIC_SERVER_FRONTEND_URL } from "$env/static/public"
 import type { Referral } from "$lib/global"
+import { fail } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
 
 export const load : PageServerLoad = ({ params }) => {
@@ -19,4 +20,28 @@ export const load : PageServerLoad = ({ params }) => {
         referralId: referralId,
         referral: response as Promise<Referral>,
     } 
+}
+
+export const actions : import('./$types').Actions = {
+    default: async ({request,cookies,params})=>{
+        const data = await request.formData()
+        if(data.get("agree") != "true"){
+            return fail(400, {
+                error: "Please tick to agree to the terms and conditions"
+            })
+        }
+        let response : Response
+        try{
+            response = await fetch(PUBLIC_SERVER_FRONTEND_URL+"/referral/"+params.referralId+"/consent?username="+cookies.get("username"),{
+                method: "POST"
+            })
+        } catch(e){
+            return fail(500,{error:"Could not submit data"})
+        }
+        if(response.status != 200){
+            const resJson = await response.json()
+            return fail(500,{error:resJson.message})
+        }
+        return
+    }
 }
--- src/routes/patient/referral/[referralId]/+page.svelte
+++ src/routes/patient/referral/[referralId]/+page.svelte
@@ -3,32 +3,17 @@
     import { PUBLIC_SERVER_FRONTEND_URL } from '$env/static/public';
     import ReferralView from '$lib/ReferralView.svelte';
     import * as Alert from '$lib/components/ui/alert';
-    import { Button } from '$lib/components/ui/button';
+    import { Button, buttonVariants } from '$lib/components/ui/button';
     import * as Card from '$lib/components/ui/card';
     import * as Table from '$lib/components/ui/table';
+    import {Input} from '$lib/components/ui/input';
     import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
     import Label from '$lib/components/ui/label/label.svelte';
     import { ReferralStatus, translateHospital, translateName } from '$lib/global';
+    import { cn } from '$lib/utils';
 
+    export let form : import('./$types').ActionData
     export let data : import('./$types').PageData;
-    let submitError = ""
-    function giveConsent(){
-        if(checked == false){
-            submitError = "Please tick to agree to the terms and conditions"
-            return
-        }
-        const response = fetch(PUBLIC_SERVER_FRONTEND_URL+"/referral/"+data.referralId+"/consent",{
-            method: "POST"
-        }).then(async (d: Response)=>{
-            if(d.status != 200){
-                submitError = (await d.json()).message
-                return
-            }
-            submitError = ""
-            invalidateAll()
-        })
-    }
-    let checked : boolean = false
 </script>
 <div class="mx-auto max-w-[40rem]">
     {#await data.referral}
@@ -41,9 +26,9 @@
             <Card.Content>
         {#if referral.ReferralStatus == ReferralStatus.Created}
             {#await translateHospital(referral.Destination)}
-                <p class="my-2">a</p>
+                <p class="my-2">Loading...</p>
             {:then destHos}
-            <div class="block my-4">
+            <form class="block my-4" method="POST">
                 <p class="my-2">I, <span class="underline">{referral.FirstName} {referral.LastName}</span> acknowledge the terms set out in this document in relation to my Personal Data:</p>
                 <ol>
                     <li class="list-decimal ml-4">That I give permission to <span class="font-bold">Ministry of Public Health</span> to transfer or
@@ -53,20 +38,20 @@
                         otherwise process said personal data in accordance with the Personal Data Protection Act (PDPA).
                         By signing this consent form, I acknowledge that these terms apply to all my personal health data.</li>
                 </ol>
-                <div class="my-4">
-                    <Checkbox class="" bind:checked={checked}></Checkbox>
-                    <Label class="mx-4 text-lg">I agree to the terms and conditions</Label>
+                <div class="my-4 flex">
+                    <Input type="checkbox" name="agree" value="true" class="box-content shrink-0 flex items-center justify-center text-current h-4 w-4 accent-primary"/>
+                    <Label class="mx-4 text-lg -mt-1">I agree to the terms and conditions</Label>
                 </div>
-                {#if submitError != ""}
+                {#if form?.error}
                     <Alert.Root class="my-4" variant="destructive">
                         <Alert.Title>Submission Error</Alert.Title>
                         <Alert.Description>
-                            {submitError}
+                            {form.error}
                         </Alert.Description>
                     </Alert.Root>
                 {/if}
-                <Button class="btn-alt block" on:click={giveConsent}>Submit</Button>
-            </div>
+                <Input type="submit" class={cn(buttonVariants(),"btn-alt block")} value="Submit"/>
+            </form>
             {/await}
         {:else if referral.ReferralStatus == ReferralStatus.Consented}
             <p class="my-2">You have given your consent. Your referral is being processed</p>
